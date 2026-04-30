9a9e2987a6ec744739043688efd6a1f4f0815ba5
BARAX <baraa.collage18@gmail.com>
2026-04-30 22:44:42 +0300

    watch page now works somewhat just need to add the cart functionality

--- Watches_view.php
+++ Watches_view.php
@@ -10,16 +10,28 @@
 <body>
     <nav class="navbar">
         <div class="nav-left">
-            <a href="index.php">Home</a>
-            <a href="SignIn.php">Sign in</a>
-            <a href="SignUp.php">Sign up</a>
-            <a href="AboutUs.php">About us</a>
+
         </div>
 
         <div class="nav-right">
-            <div class="logo"><img src="assets_img/Velmora-logo.svg"></div>
+            <div class="logo">
+                <img src="assets_img/Velmora-logo.svg">
+            </div>
+
+            <button class="menu-btn" id="menuBtn">☰</button>
         </div>
     </nav>
+
+    <div class="side-menu" id="sideMenu">
+        <button class="close-btn" id="closeMenu"></button>
+
+        <a href="index.php">Home</a>
+        <a href="SignIn.php">Sign in</a>
+        <a href="SignUp.php">Sign up</a>
+        <a href="AboutUs.php">About us</a>
+    </div>
+
+    <div class="menu-overlay" id="menuOverlay"></div>
 
     <h2 class="section-title"><?php echo htmlspecialchars($brandName); ?> Watches</h2>
 
@@ -68,6 +80,28 @@
             left: -moveAmount,
             behavior: "smooth"
         });
+    });
+    </script>
+
+    <script>
+    const menuBtn = document.getElementById("menuBtn");
+    const sideMenu = document.getElementById("sideMenu");
+    const closeMenu = document.getElementById("closeMenu");
+    const menuOverlay = document.getElementById("menuOverlay");
+
+    menuBtn.addEventListener("click", () => {
+        sideMenu.classList.add("active");
+        menuOverlay.classList.add("active");
+    });
+
+    closeMenu.addEventListener("click", () => {
+        sideMenu.classList.remove("active");
+        menuOverlay.classList.remove("active");
+    });
+
+    menuOverlay.addEventListener("click", () => {
+        sideMenu.classList.remove("active");
+        menuOverlay.classList.remove("active");
     });
     </script>
 </body>
